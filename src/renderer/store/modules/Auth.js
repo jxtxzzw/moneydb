33a0a91727d0f50d6d676d681c21ca8598f1e035
jxtxzzw <jxtxzzw@126.com>
2019-05-19 15:46:33 +0800

    路由时判断权限不足时自动跳转（又是一个大坑，有时间的话考虑用 JWT 重写这部分）

--- src/renderer/store/modules/Auth.js
+++ src/renderer/store/modules/Auth.js
@@ -1,28 +1,13 @@
-// import Vue from 'vue'
-// import router from '../../router'
-// import { API_URL } from '../../../data/config'
-
 export default {
   state: {
     isLogin: false
   },
   mutations: {
-    changeLoginStatus (state, {result}) {
-      state.isLogin = result
-    },
     setLoginStatus (state, isLogin) {
       state.isLogin = isLogin
     }
   },
   actions: {
-    // async check ({commit}) {
-    //   const result = await Vue.prototype.$http.get(API_URL + '/auth').then(data => data.data)
-    //   if (!result) {
-    //     router.push({name: '/PackageManage'})
-    //     return
-    //   }
-    //   commit('changeLoginStatus', {result})
-    // },
     logout ({commit}) {
       commit('setLoginStatus', false)
     },
--- src/server/api/Auth.js
+++ src/server/api/Auth.js
@@ -3,14 +3,20 @@ const router = express.Router()
 
 router.post('/', (req, res) => {
   const params = req.body
-  if (params.privilege === 'Login') {
+  if (params && params.privilege === 'Login') {
     if (params.username === '1' && params.password === '1') {
-      res.sendStatus(200)
+      console.log(params.to)
+      if (!params.to || params.to && params.to === 'PackageManage') {
+        res.sendStatus(200)
+      } else {
+        res.sendStatus(403)
+      }
     } else {
       res.sendStatus(403)
     }
   } else {
     // 去查权限表
+    res.sendStatus(403)
   }
 })
 
